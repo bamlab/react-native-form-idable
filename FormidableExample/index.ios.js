8b13bd4a465a3aadf8889ef52a041d40d750af66
Almouro <contact@almouro.com>
2017-10-01 18:15:48 +0200

    chore(example): add react native example

---
+++ FormidableExample/index.ios.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { DatePicker, Picker, Form, TextInput } from 'react-native-form-idable';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ddd',
+  },
+  button: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    paddingHorizontal: 10,
+    minHeight: 40,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
+
+const formStyles = {
+  fieldContainer: {
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  fieldText: {
+    color: '#333',
+    fontSize: 14,
+    fontWeight: '600',
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+  },
+};
+
+export default class FormidableExample extends Component {
+  props: PropsType;
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form
+          formStyles={formStyles}
+          onSubmit={formData => alert(JSON.stringify(formData, null, 2))}
+          toastErrors
+          onValidationError={errors => console.log(errors)}
+        >
+          <TextInput name="email" placeholder="Email" type="email" required />
+          <Picker name="language" type="language" placeholder="Language" formStyles={formStyles}>
+            <Picker.Item label="English" value="en" />
+            <Picker.Item label="French" value="fr" />
+          </Picker>
+          <DatePicker name="birthdate" type="date" placeholder="Birthdate" />
+          <TextInput name="password" placeholder="Password" type="password" required />
+          <TouchableOpacity type="submit" style={styles.button}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </Form>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('FormidableExample', () => FormidableExample);
---
+++ FormidableExample/rn-cli.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-commonjs */
+
+const path = require('path');
+const escape = require('escape-string-regexp');
+const blacklist = require('metro-bundler/src/blacklist');
+
+module.exports = {
+  extraNodeModules: {
+    react: path.resolve(__dirname, 'node_modules/react'),
+    lodash: path.resolve(__dirname, 'node_modules/lodash'),
+    'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    '@bam.tech/react-native-modalbox': path.resolve(
+      __dirname,
+      '..',
+      'node_modules/@bam.tech/react-native-modalbox',
+    ),
+  },
+  getBlacklistRE() {
+    return blacklist([
+      new RegExp(`^${escape(path.resolve(__dirname, '..', 'node_modules', 'react-native'))}\\/.*$`),
+      new RegExp(`^${escape(path.resolve(__dirname, '..', 'node_modules', 'react'))}\\/.*$`),
+    ]);
+  },
+};
